4d751c04f4feeaf4fcd3eb1aa32c60bc7e97aad3
michael <mihai@mandrescu.co>
2018-03-06 20:59:52 +0000

    Path fix.

--- start_routing_server.sh
+++ start_routing_server.sh
@@ -4,6 +4,6 @@ touch /tmp/routing_server.pid
 
 export PROD="true"
 
-uwsgi --chmod-socket=666 --uwsgi-socket /home/deploy/routing_server/routing_server.sock --wsgi-file /home/deploy/RoutingServer/routing_server/routing_server/wsgi.py
+uwsgi --chmod-socket=666 --uwsgi-socket /home/deploy/other_config/routing_server/routing_server.sock --wsgi-file /home/deploy/other_data/RoutingServer/routing_server/routing_server/wsgi.py
 
 echo $! > /tmp/routing_server.pid
