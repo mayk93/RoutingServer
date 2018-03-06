4057b79d5af56d05c9f5a6dd592804fa25dec103
michael <mihai@mandrescu.co>
2018-03-06 21:19:09 +0000

    Added virtual env flag.

--- start_routing_server.sh
+++ start_routing_server.sh
@@ -4,6 +4,6 @@ touch /tmp/routing_server.pid
 
 export PROD="true"
 
-uwsgi --chmod-socket=666 --uwsgi-socket /home/deploy/other_config/routing_server/routing_server.sock --wsgi-file /home/deploy/other_data/RoutingServer/routing_server/routing_server/wsgi.py
+uwsgi -H /home/deploy/other_env/routing_server_env --chmod-socket=666 --uwsgi-socket /home/deploy/other_config/routing_server/routing_server.sock --wsgi-file /home/deploy/other_data/RoutingServer/routing_server/routing_server/wsgi.py
 
 echo $! > /tmp/routing_server.pid
